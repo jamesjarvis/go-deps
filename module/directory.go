b424dfe2ed142296e683106e248119fe3293a2f1
James Jarvis <git@jamesjarvis.io>
2021-08-04 21:46:31 +0100

    add initial implementation of a kinda global cache thing

---
+++ module/directory.go
@@ -0,0 +1,95 @@
+package module
+
+import "golang.org/x/mod/semver"
+
+// Directory will be the global cache of seen modules, it will be responsible
+// for storing modules and ultimately resolving module version clashes.
+type Directory struct {
+	modules map[string]*VersionDirectory
+}
+
+func (d *Directory) Get(path string) *VersionDirectory {
+	vd, ok := d.modules[path]
+	if !ok {
+		return nil
+	}
+	return vd
+}
+
+func (d *Directory) GetModule(path, version string) *Module {
+	vd := d.Get(path)
+	if vd == nil {
+		return nil
+	}
+	mod := vd.GetVersion(version)
+	if mod == nil {
+		return nil
+	}
+	return mod
+}
+
+func (d *Directory) SetModule(mod *Module) *Module {
+	vd := d.Get(mod.Path)
+	if vd == nil {
+		vd = new(VersionDirectory)
+	}
+
+	fixedMod := vd.SetVersion(mod.Version, mod)
+	d.Set(mod.Path, vd)
+	return fixedMod
+}
+
+func (d *Directory) Set(path string, vd *VersionDirectory) {
+	d.modules[path] = vd
+}
+
+type VersionDirectory struct {
+	versions map[string]*Module
+}
+
+func (vd *VersionDirectory) GetVersion(version string) *Module {
+	mod, ok := vd.versions[version]
+	if !ok {
+		return nil
+	}
+	return mod
+}
+
+func (vd *VersionDirectory) SetVersion(version string, mod *Module) *Module {
+	// If this version already exists, overwrite with the new one and return.
+	if existing := vd.GetVersion(version); existing != nil {
+		vd.versions[version] = mod
+		return mod
+	}
+
+	// Get existing versions.
+	// If there is an existing version with the same major, but higher minor,
+	// then return the existing version as it is "better".
+	// If there is an existing version with the same major, but lower minor,
+	// then delete that version, and store this "better" version and return.
+	// If there is an existing version with different major, or none at all,
+	// then store this "different" version and return.
+	// 
+	// This should eventually lead to there only being one entry
+	// for each major version.
+	// A different implementation may choose to have stricter/more relaxed
+	// version resolving logic.
+	major := semver.Major(version)
+	for existingVers, existingMod := range vd.versions {
+		existingMajor := semver.Major(existingVers)
+		if major == existingMajor {
+			comparison := semver.Compare(version, existingVers)
+			// If incoming is greater than existing, replace and return.
+			if comparison > 0 {
+				delete(vd.versions, existingVers)
+				vd.versions[version] = mod
+				return mod
+			}
+			// If incoming is less than existing, return existing.
+			return existingMod
+		}
+	}
+
+	vd.versions[version] = mod
+	return mod
+}
